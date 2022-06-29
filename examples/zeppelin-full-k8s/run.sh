f58c5312e804c315d6a7fe441dc2596c73b39662
Thomas Decaux <ebuildy@gmail.com>
2022-06-29 18:55:48 +0200

    feat(examples): WIP
    fix

--- examples/zeppelin-full-k8s/run.sh
+++ examples/zeppelin-full-k8s/run.sh
@@ -6,6 +6,16 @@ THIS_DIR=./examples/zeppelin-full-k8s
 set -ex
 
 helm upgrade --install \
+    --values ${CHART_DIR}/zeppelin/values.yaml \
+    --values ${THIS_DIR}/zeppelin.yaml \
+    zep \
+    ${CHART_DIR}/zeppelin
+
+exit 0
+
+kubectl apply -f ${THIS_DIR}/zep-spark-conf-cm.yaml
+
+helm upgrade --install \
     zep-spark \
     ${CHART_DIR}/spark-artifacts
 
