5ea6b4ae91dd57f8b59929fb08900f30b15d0e3d
Thomas Decaux <ebuildy@gmail.com>
2022-08-30 14:01:50 +0200

    misc

--- examples/zeppelin-full-k8s/run.sh
+++ examples/zeppelin-full-k8s/run.sh
@@ -5,14 +5,6 @@ THIS_DIR=./examples/zeppelin-full-k8s
 
 set -ex
 
-helm upgrade --install \
-    --values ${CHART_DIR}/zeppelin/values.yaml \
-    --values ${THIS_DIR}/zeppelin.yaml \
-    zep \
-    ${CHART_DIR}/zeppelin
-
-exit 0
-
 kubectl apply -f ${THIS_DIR}/zep-spark-conf-cm.yaml
 
 helm upgrade --install \
@@ -25,14 +17,6 @@ helm upgrade --install \
     spark-spec \
     ${CHART_DIR}/spark-spec
 
-helm upgrade --install \
-    --values ${CHART_DIR}/spark-spec/values.yaml \
-    --values ${THIS_DIR}/zeppelin-interpreter-spec.yaml \
-    zep-int \
-    ${CHART_DIR}/zeppelin-interpreter-spec
+./run_zep_int_spec.sh
 
-helm upgrade --install \
-    --values ${CHART_DIR}/zeppelin/values.yaml \
-    --values ${THIS_DIR}/zeppelin.yaml \
-    zep \
-    ${CHART_DIR}/zeppelin
+./run_zep.sh
---
+++ examples/zeppelin-full-k8s/run_zep_int_spec.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CHART_DIR=./charts
+THIS_DIR=./examples/zeppelin-full-k8s
+
+set -ex
+
+helm upgrade --install \
+    --values ${CHART_DIR}/spark-spec/values.yaml \
+    --values ${THIS_DIR}/zeppelin-interpreter-spec.yaml \
+    zep-int \
+    ${CHART_DIR}/zeppelin-interpreter-spec
